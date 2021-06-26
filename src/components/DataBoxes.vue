04f37d8d6f4cf8461972f077b574888437145b25
Matthew Lin <matthewlin02@gmail.com>
2021-06-26 18:59:34 +1000

    update

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -18,7 +18,7 @@
         <!-- Box2 -->
          <div class="shadow-md bg-blue-200 p-10 text-center rounded">
         
-            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths
             </h3>
             <div class="text-2xl mb-4">
                 <span class="font-bold">New:</span>
